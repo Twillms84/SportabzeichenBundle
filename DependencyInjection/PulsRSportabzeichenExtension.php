d12843a1ae43dcff1589a029cc14534b8d9b3854
Twillms84 <timo.willms@gmail.com>
2025-12-21 23:39:29 +0100

    Update PulsRSportabzeichenExtension.php

--- DependencyInjection/PulsRSportabzeichenExtension.php
+++ DependencyInjection/PulsRSportabzeichenExtension.php
@@ -9,6 +9,22 @@ final class PulsRSportabzeichenExtension extends IServBaseExtension
     public function load(array $configs, ContainerBuilder $container): void
     {
         parent::load($configs, $container);
+
+        // IServ-kompatible Asset-Registrierung
+        $container->prependExtensionConfig('framework', [
+            'assets' => [
+                'packages' => [
+                    'pulsr-sportabzeichen' => [
+                        'json_manifest_path' => '/usr/share/iserv/web/public/assets/pulsr-sportabzeichen/manifest.json',
+                    ],
+                ],
+            ],
+        ]);
+    }
+
+    public function getAlias(): string
+    {
+        return 'pulsr_sportabzeichen';
     }
 }
 
