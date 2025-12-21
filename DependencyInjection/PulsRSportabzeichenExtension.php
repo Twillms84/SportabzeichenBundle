5955bb2a290b58ce734c9aabb459f2684ff4a751
Twillms84 <timo.willms@gmail.com>
2025-12-21 23:42:05 +0100

    Update PulsRSportabzeichenExtension.php

--- DependencyInjection/PulsRSportabzeichenExtension.php
+++ DependencyInjection/PulsRSportabzeichenExtension.php
@@ -10,18 +10,10 @@ final class PulsRSportabzeichenExtension extends IServBaseExtension
     {
         parent::load($configs, $container);
 
-<<<<<<< HEAD
-        // 🔧 IServ-kompatible Asset-Registrierung
-        $container->prependExtensionConfig('framework', [
-            'assets' => [
-                'packages' => [
-                    // Der Paketname MUSS der gleiche wie im public/assets/-Ordner sein!
-=======
         // IServ-kompatible Asset-Registrierung
         $container->prependExtensionConfig('framework', [
             'assets' => [
                 'packages' => [
->>>>>>> d12843a1ae43dcff1589a029cc14534b8d9b3854
                     'pulsr-sportabzeichen' => [
                         'json_manifest_path' => '/usr/share/iserv/web/public/assets/pulsr-sportabzeichen/manifest.json',
                     ],
@@ -32,11 +24,6 @@ final class PulsRSportabzeichenExtension extends IServBaseExtension
 
     public function getAlias(): string
     {
-<<<<<<< HEAD
-        // ⚙️ Muss exakt mit dem Bundle-Namen konform sein
-        // "pulsr_sportabzeichen" wird von IServ automatisch erkannt
-=======
->>>>>>> d12843a1ae43dcff1589a029cc14534b8d9b3854
         return 'pulsr_sportabzeichen';
     }
 }
