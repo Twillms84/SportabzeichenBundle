ea069a59b20b28fc6d3c94e8ae42dd866bc56c71
Twillms84 <timo.willms@gmail.com>
2025-12-23 15:44:00 +0100

    Update webpack.config.js

--- Resources/webpack.config.js
+++ Resources/webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-// Hier die Änderung: Wir holen das Plugin anders, um den ESM-Fehler zu vermeiden
+// Version 4 unterstützt diesen direkten Require:
 const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = {
@@ -9,14 +9,13 @@ module.exports = {
         'js/exam_results_autosave': './assets/js/exam_results_autosave.js'
     },
     output: {
-        path: path.resolve(__dirname, 'public'), // Zuerst lokal ins Modul bauen
+        path: path.resolve(__dirname, 'public'),
         filename: '[name].[contenthash:8].js',
         clean: true
     },
     plugins: [
         new WebpackManifestPlugin({
             fileName: 'manifest.json',
-            // Wichtig für Symfony: Der Pfad in der manifest.json muss relativ zum Asset-Root sein
             publicPath: 'assets/pulsr-sportabzeichen/' 
         }),
         new MiniCssExtractPlugin({
@@ -32,10 +31,6 @@ module.exports = {
                     loader: 'babel-loader',
                     options: { presets: ['@babel/preset-env'] }
                 }
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader']
             }
         ]
     }
