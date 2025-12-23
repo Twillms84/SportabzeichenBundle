c8bb579b3ae7d649452b188c9b5bef8744c212dc
Twillms84 <timo.willms@gmail.com>
2025-12-23 15:41:39 +0100

    Update webpack.config.js

--- Resources/webpack.config.js
+++ Resources/webpack.config.js
@@ -1,24 +1,23 @@
 const path = require('path');
-const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// Hier die Änderung: Wir holen das Plugin anders, um den ESM-Fehler zu vermeiden
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = {
     mode: 'production',
-    // Hier gibst du deine Quelldatei an
     entry: {
         'js/exam_results_autosave': './assets/js/exam_results_autosave.js'
     },
     output: {
-        // Ziel: Der offizielle IServ-Asset-Ordner für dein Modul
-        path: path.resolve('/usr/share/iserv/web/public/assets/pulsr-sportabzeichen'),
-        // Die URL, unter der die Dateien erreichbar sind
-        publicPath: '/assets/pulsr-sportabzeichen/',
+        path: path.resolve(__dirname, 'public'), // Zuerst lokal ins Modul bauen
         filename: '[name].[contenthash:8].js',
         clean: true
     },
     plugins: [
         new WebpackManifestPlugin({
-            fileName: 'manifest.json'
+            fileName: 'manifest.json',
+            // Wichtig für Symfony: Der Pfad in der manifest.json muss relativ zum Asset-Root sein
+            publicPath: 'assets/pulsr-sportabzeichen/' 
         }),
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash:8].css'
