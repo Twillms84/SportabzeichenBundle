190eabb64be83a8ca61e5dbe0c1d8ac9e0261b18
Twillms84 <timo.willms@gmail.com>
2026-01-03 16:39:48 +0100

    Update webpack.config.js

--- Resources/webpack.config.js
+++ Resources/webpack.config.js
@@ -6,19 +6,23 @@ module.exports = {
     mode: 'production',
     entry: {
         'js/exam_results_autosave': './assets/js/exam_results_autosave.js',
+        'js/admin_participants': './assets/js/admin_participants.js',
         'js/exam_results_scoring': './assets/js/exam_results_scoring.js',
-        'css/sportabzeichen_results': './assets/css/results.css'
+        'css/sportabzeichen_results': './assets/css/results.css',
+        'css/print_groupcard':'./assets/css/print_groupcard.css',
+        'svg/dsa_groupcard':'./assets/svg/dsa_groupcard.svg'
     },
     output: {
         path: path.resolve(__dirname, 'public'),
         filename: '[name].[contenthash:8].js',
-        clean: true
+        clean: true,
+        // HIER KOMMT DIE MAGIE FÜR ASSETS (Bilder/SVG):
+        assetModuleFilename: 'img/[name].[contenthash:8][ext]'
     },
     plugins: [
         new WebpackManifestPlugin({
             fileName: 'manifest.json',
-            // WICHTIG: Diesen Pfad so lassen, wie er in der funktionierenden Version war!
-            publicPath: 'assets/pulsr-sportabzeichen/' 
+            publicPath: 'assets/pulsr-sportabzeichen/'
         }),
         new MiniCssExtractPlugin({
             filename: '[name].[contenthash:8].css'
@@ -37,6 +41,15 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: [MiniCssExtractPlugin.loader, 'css-loader']
+            },
+            // --- NEUE RULE FÜR SVG ---
+            {
+                test: /\.svg$/,
+                type: 'asset/resource', // Kopiert die Datei und hasht sie
+                generator: {
+                    // Expliziter Output-Pfad für SVGs
+                    filename: 'img/[name].[contenthash:8][ext]'
+                }
             }
         ]
     }
