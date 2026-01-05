963dfee6d9f8fc405d328e927028a376a2fad11c
Twillms84 <timo.willms@gmail.com>
2026-01-05 22:36:23 +0100

    XVXCV

--- Repository/DisciplineRepository.php
+++ Repository/DisciplineRepository.php
@@ -11,10 +11,6 @@ use PulsR\SportabzeichenBundle\Entity\Discipline;
  */
 class DisciplineRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Discipline::class);
-    }
 
     // Hier können wir später spezielle Abfragen einbauen, z.B.:
     // "Gib mir alle Disziplinen für Kategorie 'Kraft'"
--- Repository/ExamRepository.php
+++ Repository/ExamRepository.php
@@ -11,11 +11,6 @@ use PulsR\SportabzeichenBundle\Entity\Exam;
  */
 class ExamRepository extends ServiceEntityRepository
 {
-    public function __construct(ManagerRegistry $registry)
-    {
-        parent::__construct($registry, Exam::class);
-    }
-
     // Hier könnten wir später spezielle Suchfunktionen einbauen, z.B.:
     // public function findRecentExams(int $limit): array { ... }
 }
