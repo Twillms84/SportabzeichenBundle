da8638848a85c323ea987c32c1090d57525f136e
Twillms84 <timo.willms@gmail.com>
2026-01-05 22:25:30 +0100

    Update ExamDashboardController.php

--- Controller/ExamDashboardController.php
+++ Controller/ExamDashboardController.php
@@ -6,6 +6,7 @@ namespace PulsR\SportabzeichenBundle\Controller;
 
 use Doctrine\DBAL\Connection;
 use IServ\CoreBundle\Controller\AbstractPageController;
+use PulsR\SportabzeichenBundle\Repository\ExamRepository; // <--- Neu
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -16,37 +17,29 @@ final class ExamDashboardController extends AbstractPageController
     #[Route('', name: 'dashboard', methods: ['GET'])]
     public function dashboard(
         Request $request,
+        ExamRepository $examRepository, // <--- Repository injizieren
         Connection $conn
     ): Response {
         $this->denyAccessUnlessGranted('PRIV_SPORTABZEICHEN_RESULTS');
 
         /* ------------------------------------------------------------
-         * Prüfungen laden
+         * Prüfungen als ENTITIES laden (statt SQL)
          * ------------------------------------------------------------ */
-        $exams = $conn->fetchAllAssociative(
-            'SELECT id, exam_name, exam_year, exam_date
-             FROM sportabzeichen_exams
-             ORDER BY exam_year DESC, exam_date DESC'
-        );
+        // Holt Objekte zurück. Wir sortieren nach Jahr und Datum absteigend.
+        // Achtung: In der Entity müssen die Felder 'year' und 'date' heißen.
+        $exams = $examRepository->findBy([], ['year' => 'DESC', 'date' => 'DESC']);
 
         $examId = $request->query->getInt('exam');
-
         $participants = [];
 
+        // Teilnehmer laden wir erstmal weiter per SQL, das ist okay für die Performance
         if ($examId > 0) {
             $participants = $conn->fetchAllAssociative(
-                <<<SQL
-                SELECT
-                    p.id,
-                    p.import_id,
-                    p.geschlecht,
-                    p.geburtsdatum
-                FROM sportabzeichen_exam_participants ep
-                JOIN sportabzeichen_participants p
-                    ON p.id = ep.participant_id
-                WHERE ep.exam_id = ?
-                ORDER BY p.import_id
-                SQL,
+                'SELECT p.id, p.import_id, p.geschlecht, p.geburtsdatum
+                 FROM sportabzeichen_exam_participants ep
+                 JOIN sportabzeichen_participants p ON p.id = ep.participant_id
+                 WHERE ep.exam_id = ?
+                 ORDER BY p.import_id',
                 [$examId]
             );
         }
