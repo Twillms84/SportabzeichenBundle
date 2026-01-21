a89244905c9f3fad9cb755ed87e68a85f5b5000a
Twillms84 <timo.willms@gmail.com>
2026-01-21 10:59:45 +0100

    Update SportabzeichenService.php

--- Service/SportabzeichenService.php
+++ Service/SportabzeichenService.php
@@ -105,7 +105,7 @@ class SportabzeichenService
                 $this->em->persist($proof);
                 
                 // WICHTIG: Damit syncSummary im gleichen Request den Nachweis findet:
-                $proof->setParticipant($participant)
+                $proof->setParticipant($participant);
                 $this->em->persist($proof);
                 $this->em->flush();
             }
